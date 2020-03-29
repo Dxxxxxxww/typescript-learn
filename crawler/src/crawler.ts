cf4c7cbf8a595e6a623d076b4d57959c496c12a8
Dxxxxxxww <gotu2018@163.com>
2020-03-29 23:20:28 +0800

    增加 生成JSON并写入文件，分割代码，一个函数只做一件事 by dxw

--- crawler/src/crawler.ts
+++ crawler/src/crawler.ts
@@ -1,3 +1,7 @@
+// node 模块
+import fs from 'fs'
+// node 模块
+import path from 'path'
 // ts文件直接引用js文件会有问题，需要翻译文件，一般都是叫做 .d.ts 结尾的文件,这里需要安装 @types/superagent
 // superagent 可以当做是一个 axios 一样的发请求的工具，但是可以爬取到渲染的内容
 import superagent from 'superagent'
@@ -7,6 +11,15 @@ import cheerio from 'cheerio'
 interface Course {
   title: string
   count: number
+}
+
+interface CourseResult {
+  time: number
+  data: Course[]
+}
+
+interface Content {
+  [propName: number]: Course[]
 }
 
 class Crawler {
@@ -43,11 +56,21 @@ class Crawler {
     }
   }
   async initSpiderProcess() {
+    const filePath = path.resolve(__dirname, '../data/course.json')
     const html = await this.getRawHtml()
-    const courseResult = this.getCourseInfo(html)
+    const courseInfo = this.getCourseInfo(html)
+    const fileContent = this.generateJSONContent(courseInfo)
+    fs.writeFileSync(filePath, JSON.stringify(fileContent))
   }
-  generateJSONContent() {
-    
+  // 一个函数就只有一个功能，这个函数是生成 JSON 的，就不要让它来写入文件了
+  generateJSONContent(courseInfo: CourseResult) {
+    const filePath = path.resolve(__dirname, '../data/course.json')
+    let fileContent: Content = {}
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+    }
+    fileContent[courseInfo.time] = courseInfo.data
+    return fileContent
   }
 }
 
